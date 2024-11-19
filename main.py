7c0f55efd12f657ec09063a076fdb006f0d1f99c
Angelo Hu <angelohu0402@gmail.com>
2024-11-19 13:16:58 -0600

    First commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# Collects user input for amount of money they wish to deposit
+def deposit():
+    # Continue to ask for deposit until there is enough money to play
+    while True:
+        deposit_amount = input("How much would you like to depost? $")
